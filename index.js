dc636cfa58f0b43989aff2e667b4139f0435f4d0
ashiyaman <ashiya1005@gmail.com>
2025-08-05 16:43:45 +0530

    change

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const corsOptions = {
-  origin: "*",
   credentials: true,
   optionSuccessStatus: 200,
 };
